f8f3c13049d8971fe39cf29119cc7210b999fc33
BreadKramz <marklovegames55@gmail.com>
2026-06-17 22:38:46 +0800

    added other stuff

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ function addExpense() {
     }
 
     document.getElementById('expenseDescription').innerHTML += description + '<br><hr>';
-    document.getElementById('expenseAmount').innerHTML += '₱' + amount.toFixed(2) + '<br><hr>';
-    document.getElementById('expenseResult').innerHTML += total + ' - ' + amount + ' = ' + (total-amount) + '<br><hr>';
+    document.getElementById('expenseAmount').innerHTML += '₱' + formatMoney(amount) + '<br><hr>';
+    document.getElementById('expenseResult').innerHTML += formatMoney(total) + ' - ' + formatMoney(amount) + ' = ' + formatMoney(total-amount) + '<br><hr>';
     
     total = total - amount;
     
-    document.getElementById('amountResult').innerHTML = '₱ ' + total;
+    document.getElementById('amountResult').innerHTML = '₱ ' + formatMoney(total);
     document.getElementById('description').value = '';
     document.getElementById('amount').value = '';
 }  
@@ -35,6 +35,15 @@ function addTotalAmount() {
 
     total = total + addedAmount;
 
-    document.getElementById('amountResult').innerHTML = '₱ ' + total.toFixed(2);
+    document.getElementById('amountResult').innerHTML = '₱ ' + formatMoney(total);
     document.getElementById('totalAmount').value = '';
+}
+
+toLocaleString()
+
+function formatMoney(value) {
+    return Number(value).toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
 }
