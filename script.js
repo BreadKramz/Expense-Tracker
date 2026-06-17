aa46201f51f50cf65d47c8ac71cb5e1b0fbcce75
BreadKramz <marklovegames55@gmail.com>
2026-06-17 21:45:32 +0800

    first commit for expense tracker system

---
+++ script.js
@@ -0,0 +1,36 @@
+let total = 0;
+
+function addExpense() {
+    const description = document.getElementById('description').value;
+    const amount = Number(document.getElementById('amount').value);
+
+    if (!amount) return;
+
+    document.getElementById('alerts').innerHTML = '';
+
+    if (amount > total) {
+        document.getElementById('alerts').innerHTML = 'Insufficient funds!';
+        return;
+    }
+
+    document.getElementById('expenseDescription').innerHTML += description + '<br><hr>';
+    document.getElementById('expenseAmount').innerHTML += '₱' + amount + '<br><hr>';
+    document.getElementById('expenseResult').innerHTML += total + ' - ' + amount + ' = ' + (total-amount) + '<br><hr>';
+    
+    total = total - amount;
+    
+    document.getElementById('amountResult').innerHTML = 'Total Amount: ₱ ' + total;
+    document.getElementById('description').value = '';
+    document.getElementById('amount').value = '';
+}  
+
+function addTotalAmount() {
+    const addedAmount = Number(document.getElementById('totalAmount').value);
+
+    if (!addedAmount) return;
+
+    total = total + addedAmount;
+
+    document.getElementById('amountResult').innerHTML = 'Total Amount: ₱ ' + total;
+    document.getElementById('totalAmount').value = '';
+}
