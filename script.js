3b8222f2c8f4020fbfe8e8148c0493a59fd2435b
BreadKramz <marklovegames55@gmail.com>
2026-06-17 22:43:52 +0800

    ui principle for mobile

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let total = 0;
 
+// allow letters only in description
 document.getElementById('description').addEventListener('input', function () {
     this.value = this.value.replace(/[^A-Za-z ]/g, '');
 });
 
+// format money (1,000.00)
+function formatMoney(value) {
+    return Number(value).toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+}
+
 function addExpense() {
+
     const description = document.getElementById('description').value;
     const amount = Number(document.getElementById('amount').value);
 
@@ -19,16 +29,19 @@ function addExpense() {
 
     document.getElementById('expenseDescription').innerHTML += description + '<br><hr>';
     document.getElementById('expenseAmount').innerHTML += '₱' + formatMoney(amount) + '<br><hr>';
-    document.getElementById('expenseResult').innerHTML += formatMoney(total) + ' - ' + formatMoney(amount) + ' = ' + formatMoney(total-amount) + '<br><hr>';
-    
+    document.getElementById('expenseResult').innerHTML +=
+        formatMoney(total) + ' - ' + formatMoney(amount) + ' = ' + formatMoney(total - amount) + '<br><hr>';
+
     total = total - amount;
-    
+
     document.getElementById('amountResult').innerHTML = '₱ ' + formatMoney(total);
+
     document.getElementById('description').value = '';
     document.getElementById('amount').value = '';
-}  
+}
 
 function addTotalAmount() {
+
     const addedAmount = Number(document.getElementById('totalAmount').value);
 
     if (!addedAmount) return;
@@ -36,14 +49,6 @@ function addTotalAmount() {
     total = total + addedAmount;
 
     document.getElementById('amountResult').innerHTML = '₱ ' + formatMoney(total);
+
     document.getElementById('totalAmount').value = '';
-}
-
-toLocaleString()
-
-function formatMoney(value) {
-    return Number(value).toLocaleString('en-US', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-    });
 }
